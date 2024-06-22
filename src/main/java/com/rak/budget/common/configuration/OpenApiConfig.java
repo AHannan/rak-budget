77d6c99da2a63463e83549d29fac0ddb38ec1005
Abdul Hannan <ahannan@seedz.tech>
2024-06-23 02:58:57 +0500

    Swagger included

---
+++ src/main/java/com/rak/budget/common/configuration/OpenApiConfig.java
@@ -0,0 +1,19 @@
+//package com.rak.budget.common.configuration;
+//
+//import io.swagger.v3.oas.models.OpenAPI;
+//import io.swagger.v3.oas.models.info.Info;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class OpenApiConfig {
+//
+//    @Bean
+//    public OpenAPI customOpenAPI() {
+//        return new OpenAPI()
+//                .info(new Info()
+//                        .title("Budget Service API")
+//                        .version("1.0")
+//                        .description("API documentation for Budget Service"));
+//    }
+//}
