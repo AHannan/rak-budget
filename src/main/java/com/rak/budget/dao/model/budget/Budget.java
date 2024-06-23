828900e87f9f4daae57da98b7554894fa574626d
Abdul Hannan <ahannan@seedz.tech>
2024-06-23 18:07:46 +0500

    Using H2

--- src/main/java/com/rak/budget/dao/model/budget/Budget.java
+++ src/main/java/com/rak/budget/dao/model/budget/Budget.java
@@ -1,29 +1,36 @@
 package com.rak.budget.dao.model.budget;
 
-import lombok.Data;
-import org.hibernate.annotations.GenericGenerator;
-
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.GenericGenerator;
+
 import java.math.BigDecimal;
 
 @Data
 @Entity
-@Table(name = "budget", schema = "budget")
+@Table(name = "budget")
+@EqualsAndHashCode(of = {"id", "userId", "amount", "category"})
 public class Budget {
     @Id
     @GeneratedValue(generator = "uuid")
     @GenericGenerator(name = "uuid", strategy = "uuid2")
     private String id;
+
+    @Column(nullable = false)
     private String userId;
+
+    @Column(nullable = false, precision = 10, scale = 2)
     private BigDecimal amount;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "category_id", nullable = false)
     private BudgetCategory category;
-
 }
--- src/main/java/com/rak/budget/dao/model/budget/BudgetCategory.java
+++ src/main/java/com/rak/budget/dao/model/budget/BudgetCategory.java
@@ -1,21 +1,25 @@
 package com.rak.budget.dao.model.budget;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import org.hibernate.annotations.GenericGenerator;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.Column;
 
 @Data
 @Entity
-@Table(name = "budget_category", schema = "budget")
+@Table(name = "budget_category")
+@EqualsAndHashCode(of = {"id", "name"})
 public class BudgetCategory {
     @Id
     @GeneratedValue(generator = "uuid")
     @GenericGenerator(name = "uuid", strategy = "uuid2")
     private String id;
-    private String name;
 
+    @Column(nullable = false)
+    private String name;
 }
